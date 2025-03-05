a92926223a24774bfe691e453f235db6406b0c6f
junsheng_wang <build@build-server>
2025-03-05 10:34:36 +0800

    hw01 support confirm generated db
    Signed-off-by: junsheng_wang <build@build-server>

--- student_assignment.py
+++ student_assignment.py
@@ -12,7 +12,7 @@ gpt_emb_config = get_model_configuration(gpt_emb_version)
 
 dbpath = "./"
 
-def generate_hw01():
+def generate_collection():
     chroma_client = chromadb.PersistentClient(path=dbpath)
     openai_ef = embedding_functions.OpenAIEmbeddingFunction(
         api_key=gpt_emb_config['api_key'],
@@ -58,7 +58,28 @@ def generate_hw01():
     )
 
     return collection
-    
+
+def generate_hw01():
+    chroma_client = chromadb.PersistentClient(path=dbpath)
+    openai_ef = embedding_functions.OpenAIEmbeddingFunction(
+        api_key=gpt_emb_config['api_key'],
+        api_base=gpt_emb_config['api_base'],
+        api_type=gpt_emb_config['openai_type'],
+        api_version=gpt_emb_config['api_version'],
+        deployment_id=gpt_emb_config['deployment_name']
+    )
+    collections = chroma_client.list_collections()
+    if "TRAVEL" not in collections:
+        generate_collection()
+    collection = chroma_client.get_collection(
+        name="TRAVEL",
+        embedding_function=openai_ef
+    )
+
+    return collection
+
+
+
 def generate_hw02(question, city, store_type, start_date, end_date):
     chroma_client = chromadb.PersistentClient(path=dbpath)
     openai_ef = embedding_functions.OpenAIEmbeddingFunction(
